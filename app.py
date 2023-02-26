1681f7624f44e2124d98a322ca5ac0e6a9ab7b48
Udaykiran <108007033+udaykiran927@users.noreply.github.com>
2023-02-26 13:07:16 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,13 +1,4 @@
 from flask import Flask,render_template,request,Response
-import os
-import knn
-import mnb
-import knn_classify
-import mnb_classify
-import extract as ex
-from mail import Mail
-from pprint import pprint
-from getpass import getpass
 
 app=Flask(__name__)
 @app.route('/')
